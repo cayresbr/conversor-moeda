632840a055d6d68da036f0bdf92d45f31b1c8787
cayresbr <rafaelcayress@gmail.com>
2025-08-01 23:03:54 -0600

    adicionado alteracao da moeda

--- scripts.js
+++ scripts.js
@@ -1,27 +1,44 @@
-const convertButton = document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button");
+const currencyType = document.querySelector(".currency-type");
 
-function convertValues(){
+function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
 
-  const inputCurrencyValue = document.querySelector(".input-currency").value
+  const currencyValueToConvert = document.querySelector(
+    ".currencyValueToConvert"
+  );
 
-  const currencyValueToConvert = document.querySelector(".currencyValueToConvert")
+  const currencyValueConverted = document.querySelector(
+    ".currencyValueConverted"
+  );
 
-  const currencyValueConverted = document.querySelector(".currencyValueConverted")
+  const dolarToday = 5.5;
+  const euroToday = 7;
+  const libraToday = 8;
 
-  const dolarToday = 5.5
-
-  const convertedValue = inputCurrencyValue / dolarToday
+  if (currencyType.value == "dolar") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+  if (currencyType.value == "euro") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR"
+    }).format(inputCurrencyValue / euroToday)
+  }
+  if (currencyType.value == "libra") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP"
+    }).format(inputCurrencyValue / libraToday)
+  }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
-    currency: "BRL"
-  }).format(inputCurrencyValue)
-
-  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD"
-  }).format(convertedValue)
+    currency: "BRL",
+  }).format(inputCurrencyValue);
 }
 
-convertButton.addEventListener("click", convertValues)
-
+convertButton.addEventListener("click", convertValues);
